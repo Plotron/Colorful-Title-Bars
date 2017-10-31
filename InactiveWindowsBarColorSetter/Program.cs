f943c8643ef4acfd8331cc210805524d0c97229d
Wojciech Prokurat <wojtekprokurat@outlook.com>
2017-10-31 18:47:58 +0100

    testing GIT

--- InactiveWindowsBarColorSetter/Program.cs
+++ InactiveWindowsBarColorSetter/Program.cs
@@ -14,6 +14,7 @@ namespace InactiveWindowsBarColorSetter
         [STAThread]
         static void Main()
         {
+            //testing GIT
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
